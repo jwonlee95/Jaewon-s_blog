35b45147b2d114c9811340a1814fdd666c8e212a
jwonlee95 <96804163+jwonlee95@users.noreply.github.com>
2022-03-06 07:32:40 +0900

    Project Done

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { useRouter } from 'next/router'
 import { getPosts, getPostDetails } from '../../services'
 
 import {
@@ -9,9 +9,15 @@ import {
   Author,
   Comments,
   CommentsForm,
+  Loader,
 } from '../../components'
 
 const PostDetails = ({ post }) => {
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return <Loader />
+  }
   return (
     <div className="container mx-auto mb-8 px-10">
       <div className="gird grid-cols-1 gap-12 lg:grid-cols-12">
@@ -50,6 +56,6 @@ export async function getStaticPaths() {
 
   return {
     paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
-    fallback: false,
+    fallback: true,
   }
 }
